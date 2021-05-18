ece58541bb4743a08fce939468dcaa76bdb8dad6
Justin Szaday <wingpad@gmail.com>
2021-05-17 21:59:00 -0500

    write example

--- secdest/secdest.C
+++ secdest/secdest.C
@@ -1,30 +1,66 @@
 #include <ck.h>
 
+#ifndef DECOMP_FACTOR
+#define DECOMP_FACTOR 4
+#endif
+
+#ifndef RECREATE_SECTION
+#define RECREATE_SECTION 0
+#endif
+
+#ifndef REFRESH_SECTION
+#define REFRESH_SECTION 0
+#endif
+
+constexpr auto kDecompFactor = DECOMP_FACTOR;
+constexpr auto kRecreateSection = static_cast<bool>(RECREATE_SECTION);
+constexpr auto kRefreshSection = static_cast<bool>(REFRESH_SECTION);
+
 #include "secdest.decl.h"
+
+CProxy_Main mainProxy;
+
+struct cookie_msg : public CkMcastBaseMsg, public CMessage_cookie_msg {
+  int numIters;
+
+  cookie_msg(const int& _1) : numIters(_1) {}
+
+  void pup(PUP::er& p) {
+    CMessage_cookie_msg::pup(p);
+    p | numIters;
+  }
+};
 
 struct Main : public CBase_Main {
   int numIters, numReps;
-  CProxy_Receiver receivers;
-  CProxy_Sender senders;
 
-  Main(CkArgMsg *m)
-  : numIters(atoi(m->argv[1])), numReps(numIters / 2 + 1) {
+  Main(CkArgMsg* m) : numIters(atoi(m->argv[1])), numReps(numIters / 2 + 1) {
     if (numReps > 129) numReps = 129;
+    mainProxy = thisProxy;
 
-    receivers = CProxy_Receiver::ckNew(numIters);
-    senders = CProxy_Sender::ckNew(numIters, receivers);
+    auto numChares = kDecompFactor * CkNumPes();
+    CkCallback cb(CkIndex_Main::run(NULL), thisProxy);
 
-    thisProxy.run();
+    CkPrintf("main> kRecreateSection=%d, kRefreshSection=%d\n",
+             kRecreateSection, kRefreshSection);
+
+    CkPrintf("main> kDecompFactor=%d, kNumPes=%d\n", kDecompFactor, CkNumPes());
+
+    CProxy_Contributor::ckNew(numChares, numChares, cb);
   }
 
-  void run(void) {
+  void run(CkArrayCreatedMsg* msg) {
+    CProxy_Contributor contributors(msg->aid);
     double avgTime = 0.0;
 
     for (int i = 0; i < numReps; i += 1) {
+      if ((i % 2) == 0) {
+        CkPrintf("main> repetition %d of %d\n", i + 1, numReps);
+      }
+
       auto start = CkWallTimer();
 
-      senders.send();
-      receivers.run();
+      contributors[0].run(numIters);
 
       CkWaitQD();
 
@@ -33,35 +69,49 @@ struct Main : public CBase_Main {
       avgTime += end - start;
     }
 
-    CkPrintf("on avg, exchange took: %f s\n", avgTime / numReps);
+    CkPrintf("main> on average, each batch of %d iterations took: %f s\n", numIters, avgTime / numReps);
 
     CkExit();
   }
 };
 
-class Receiver: public CBase_Receiver {
-  Receiver_SDAG_CODE;
-public:
-  int i, j;
-  int numIters;
+class Contributor : public CBase_Contributor {
+  Contributor_SDAG_CODE;
 
-  Receiver(const int& _1): numIters(_1) {} 
-};
+ public:
+  CProxySection_Contributor secProxy;
+  CkSectionInfo cookie;
 
-class Sender: public CBase_Sender {
-  Sender_SDAG_CODE;
-public:
-  int numIters;
-  CProxy_Receiver receivers;
+  int factor;
+  int numChares, numIters, it;
 
-  Sender(const int& _1, const CProxy_Receiver& _2)
-  : numIters(_1), receivers(_2) {}
+  Contributor(const int& _1) : numChares(_1) {
+    if (thisIndex == 0 && !kRecreateSection) {
+      this->create_section();
+    }
+  }
 
-  void send(void) {
-    for (int i = 0; i < numIters; i++) {
-      for (int j = 0; j < CkNumPes(); j++) {
-        receivers[j].receive(i);
-      }
+  void create_section(void) {
+    this->factor = 0;
+    std::vector<CkArrayIndex> elems;  // add array indices
+    elems.reserve(numChares / 2);
+    for (auto i = 0; i < numChares; i += 2) {
+      elems.emplace_back(i);
+      factor += i;
+    }
+    this->secProxy =
+        CProxySection_Contributor::ckNew(this->ckGetArrayID(), elems);
+  }
+
+  void run(cookie_msg* msg) {
+    CkCallback cb(CkReductionTarget(Contributor, redn_done), thisProxy[0]);
+    this->numIters = msg->numIters;
+    CkGetSectionInfo(this->cookie, msg);
+
+    for (auto it = 0; it < this->numIters; it += 1) {
+      auto data = it * this->thisIndex;
+      CProxySection_Contributor::contribute(
+          sizeof(int), &data, CkReduction::sum_int, this->cookie, cb);
     }
   }
 };
