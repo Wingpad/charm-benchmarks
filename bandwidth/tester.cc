17578f978a863de9cebba3ff3dba6b892d8338a4
Justin Szaday <wingpad@gmail.com>
2021-10-05 23:22:15 -0400

    Restyle

--- bandwidth/tester.cc
+++ bandwidth/tester.cc
@@ -12,24 +12,40 @@ struct Main : public CBase_Main {
   double *data;
   double start, totalTime;
 
+  std::deque<int> sizes;
+
   CProxy_Receiver receiver;
 
   Main(CkArgMsg *m) {
-    CkEnforceMsg(m->argc >= 3, "expected nIters and nElts");
+    CkEnforceMsg(m->argc >= 2, "expected nIters and nElts");
 
     this->nIters = atoi(m->argv[1]);
-    this->nReps = (nIters / 10) + 1;
+    this->nReps = (m->argc >= 3) ? atoi(m->argv[2]) : (nIters / 10) + 1;
     this->nSkip = nReps / 2;
-
-    auto nElts = atoi(m->argv[2]);
 
     auto whichNode = CkNumNodes() - 1;
     auto whichPe = (whichNode == CkMyNode()) ? ((CkMyPe() + 1) % CkNumPes())
                                              : CkNodeFirst(whichNode);
-    receiver = CProxy_Receiver::ckNew(whichPe);
+
+    auto maxSize = 512 * 1024;
+    for (auto size = 1; size <= maxSize; size *= 2) {
+      this->sizes.push_back(size);
+    }
+
+    CkPrintf("Size\t\tMB/s\n");
 
     mainProxy = thisProxy;
-    thisProxy.run(nElts * 1024);
+    receiver = CProxy_Receiver::ckNew(whichPe);
+  }
+
+  void next(void) {
+    if (sizes.empty()) {
+      CkExit();
+    } else {
+      auto size = sizes.front();
+      thisProxy.run(size);
+      sizes.pop_front();
+    }
   }
 };
 
@@ -38,7 +54,7 @@ class Receiver : public CBase_Receiver {
   Receiver_SDAG_CODE;
 
  public:
-  Receiver(void) {}
+  Receiver(void) { mainProxy.next(); }
 };
 
 #include "tester.def.h"
