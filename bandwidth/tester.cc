167d2b6797b80de7d59ab1d271d6a1841b2eab4b
Justin Szaday <jjszaday@gmail.com>
2021-10-07 16:57:02 -0400

    Add !USE_SDAG Codepath

--- bandwidth/tester.cc
+++ bandwidth/tester.cc
@@ -9,14 +9,15 @@ struct Main : public CBase_Main {
   int nIters, nReps, nSkip;
   int rep;
 
-  double *data;
+  double* data;
   double start, totalTime;
 
   std::deque<int> sizes;
 
   CProxy_Receiver receiver;
+  CthThread th;
 
-  Main(CkArgMsg *m) {
+  Main(CkArgMsg* m) {
     CkEnforceMsg(m->argc >= 2, "expected nIters and nElts");
 
     this->nIters = atoi(m->argv[1]);
@@ -47,14 +48,75 @@ struct Main : public CBase_Main {
       sizes.pop_front();
     }
   }
+
+  inline void initialize(const int& size) {
+    this->data = new double[size];
+    this->totalTime = 0;
+    this->th = CthSelf();
+  }
+
+  inline void finalize(const int& size) {
+    auto avgTime = this->totalTime / this->nReps;
+    auto totalSize = size * sizeof(double);
+    auto bw = (totalSize * this->nIters) / (avgTime * 1024.0 * 1024.0);
+    CkPrintf("%lu\t\t%g\n", totalSize, bw);
+    delete[] this->data;
+    this->th = nullptr;
+    thisProxy.next();
+  }
+
+#if !USE_SDAG
+  void run(int size) {
+    this->initialize(size);
+
+    for (rep = 0; rep < (nReps + nSkip); rep += 1) {
+      this->start = CkWallTimer();
+      receiver.run(nIters);
+      for (auto it = 0; it < nIters; it += 1) {
+        receiver.arrival(size, this->data);
+      }
+      CthSuspend();
+      auto end = CkWallTimer();
+      auto time = end - start;
+      if (rep >= nSkip) {
+        this->totalTime += time;
+      }
+    }
+
+    this->finalize(size);
+  }
+
+  void completion(void) { CthAwaken(this->th); }
+#endif
 };
 
 class Receiver : public CBase_Receiver {
   int it;
+  int nIters;
   Receiver_SDAG_CODE;
 
  public:
-  Receiver(void) { mainProxy.next(); }
+  Receiver(void) : it(0), nIters(0) { mainProxy.next(); }
+
+#if !USE_SDAG
+  inline void run(const int& nMsgs) {
+    this->nIters += nMsgs;
+    this->arrival(0, nullptr, false);
+  }
+
+  inline void arrival(const int& size, double* data,
+                      const bool& increment = true) {
+    if (increment) {
+      this->it += 1;
+    }
+
+    if ((this->it - this->nIters) == 0) {
+      this->it = 0;
+      this->nIters = 0;
+      mainProxy.completion();
+    }
+  }
+#endif
 };
 
 #include "tester.def.h"
