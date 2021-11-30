2f6a17d966ebc4874b559cb2d31d6174fc73267c
Justin Szaday <jjszaday@gmail.com>
2021-11-30 11:04:19 -0500

    add small ult timing program

---
+++ timeult/tester.cc
@@ -0,0 +1,74 @@
+#include "tester.hh"
+
+void handleMsg(void *msg) {
+  if (++CpvAccess(it) >= CpvAccess(nIters)) {
+    CmiSetHandler(msg, CpvAccess(doneHandlerIdx));
+  } else {
+    CmiSetHandler(msg, CpvAccess(msgHandlerIdx));
+  }
+
+  CmiSyncSendAndFree(CmiMyPe(), sizeof(EmptyMsg), (char *)msg);
+}
+
+void handleDone(void *msg) {
+  auto time = CmiWallTimer() - CpvAccess(startTime);
+  auto &totalTime = CpvAccess(totalTime);
+  totalTime += time;
+
+  CpvAccess(it) = 0;
+
+  if (++CpvAccess(rep) >= CpvAccess(nReps)) {
+    const char *phase = CpvAccess(phase) ? "thread" : "handler";
+    CmiPrintf("%d> average time for %d %s switches was %g us.\n", CmiMyPe(),
+              CpvAccess(nIters), phase, (1e6 * totalTime) / CpvAccess(nReps));
+    if (++CpvAccess(phase) == 1) {
+      auto th = CthCreate((CthVoidFn)runThread, msg, 0);
+      CthAwaken(th);
+    } else {
+      CmiFree(msg);
+      CsdExitScheduler();
+    }
+  } else {
+    CpvAccess(startTime) = CmiWallTimer();
+    CmiSetHandler(msg, CpvAccess(msgHandlerIdx));
+    CmiSyncSendAndFree(CmiMyPe(), sizeof(EmptyMsg), (char *)msg);
+  }
+}
+
+void runThread(void* msg) {
+  CpvAccess(startTime) = CmiWallTimer();
+
+  while (++CpvAccess(it) >= CpvAccess(nIters)) {
+    CthYield();
+  }
+
+  CmiSetHandler(msg, CpvAccess(doneHandlerIdx));
+  CmiSyncSendAndFree(CmiMyPe(), sizeof(EmptyMsg), (char *)msg);
+}
+
+void handleInit(int argc, char **argv) {
+  // Call CpvInitialize on all pseudo-globals
+  initializeGlobals();
+  // Register handlers
+  CpvAccess(msgHandlerIdx) = CmiRegisterHandler((CmiHandler)handleMsg);
+  CpvAccess(doneHandlerIdx) = CmiRegisterHandler((CmiHandler)handleDone);
+  // Setup arguments
+  CpvAccess(it) = 0;
+  CpvAccess(rep) = 0;
+  CpvAccess(phase) = 0;
+  CpvAccess(nReps) = 11;
+  CpvAccess(nIters) = 128;
+  // Setup timing
+  CpvAccess(totalTime) = 0;
+  CpvAccess(startTime) = CmiWallTimer();
+  // GO--!
+  if (CmiMyPe() == 0) {
+    EmptyMsg msg;
+    CmiSetHandler(&msg, CpvAccess(msgHandlerIdx));
+    CmiSyncSend(CmiMyPe(), sizeof(EmptyMsg), reinterpret_cast<char *>(&msg));
+  } else {
+    CsdExitScheduler();
+  }
+}
+
+int main(int argc, char **argv) { ConverseInit(argc, argv, handleInit, 0, 0); }
